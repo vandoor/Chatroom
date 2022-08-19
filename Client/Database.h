dd1e70730946470383c8f5b417680a93d6eb8e00
vandoor <1506127307@qq.com>
2022-08-20 00:23:10 +0800

    8.20

--- Client/Database.h
+++ Client/Database.h
@@ -9,9 +9,14 @@ public:
 	static int nRows;
     int Register(const char *,const char *);
 	int CreateGroup(const char*,const char*);	
+	int AskFriend(const char*,const char*);
+	int Join(const char*,const char*);
+	int Invite(const char*,const char*,const char*);
 private:
     sqlite3 * handler;//句柄
-    char * mesg;//错误信息
+	char * order;
+    char * errormsg;//错误信息
     int val;//用来判断各种错误
 	int RandomID(const char*);
+	int Count(const char*,const char*, const char*);
 };
--- Client/main.cpp
+++ Client/main.cpp
@@ -32,39 +32,35 @@ int CODEFOR(const string&s){
 void *ProcessInput(void*id)
 {
 	int cid=*(int*)id, op;
+	cout<<cid<<' '<<"joined\n";
 	char buf[BUFFER_SIZE], buf1[BUFFER_SIZE], buf2[BUFFER_SIZE];
     char ID[BUFFER_SIZE], ID2[BUFFER_SIZE];
 	Database DB;
 	while(true)
 	{
-		int ret=recv(cid, buf, BUFFER_SIZE, 0);
-		if(ret<=0){
+		int rec=recv(cid, buf, BUFFER_SIZE, 0);
+		if(rec<=0){
+			cout<<cid<<' '<<"exit"<<hvie;
 			pthread_exit(NULL);
 		}
 		//sscanf(buf, "# %d | %s | %s", &op, buf1, buf2);
 		cout<<buf<<"\n";
-		neb::CJsonObject job(buf);
-		vector<string>Keys;
-		vector<string>Values;
-		while(1){
-			string k,v;
-			int gk=job.GetKey(k);
-			if(!gk) break;
-			if(job.Get(k,v)){
-				Keys.push_back(k);
-				Values.push_back(v);
-			}
-		}
-		int n=Keys.size();
-		if(!n) continue;
-		for(int i=0;i<n;i++) cout<<Keys[i]<<':'<<Values[i]<<"\n";
-		cout<<Values[0]<<' '<<CODEFOR(Values[0])<<endl;
-		switch(CODEFOR(Values[0])){
+		neb::CJsonObject job(buf), ret;
+		string tmp, s1, s2, type;
+		cout<<job.ToString()<<endl;
+		if(!job.Get("type", type)) continue;
+		switch(CODEFOR(type)){
 			case 1:{
 				//Register;
 				cout<<"register!\n";
-				int UID=DB.Register(Values[1].c_str(), Values[2].c_str());
-				cout<<UID<<'\n';	
+				job.Get("name", s1);
+				job.Get("password", s2);
+				int UID=DB.Register(s1.c_str(),
+					   				s2.c_str());
+				cout<<UID<<'\n';
+				ret.Add("type", type);
+				ret.Add("status", UID>=0);
+				ret.Add("ID", UID);
 				break;
 			}		   
 			case 2:{
@@ -73,30 +69,83 @@ void *ProcessInput(void*id)
 			}		   
 			case 3:{
 				//Create Group
-				 break;
-			}		   
+				cout<<"create group!\n";
+				job.Get("UserID", s1);
+				job.Get("GroupName", s2);
+			    int GroupID=DB.CreateGroup(s1.c_str(),s2.c_str());
+				cout<<"GroupID "<<GroupID<<endl;
+				ret.Add("type", type);
+				ret.Add("status", GroupID>=0);
+				ret.Add("ID", GroupID);
+				break;
+			}
 			case 4:{
-				//AddFriend or AddToGroup
-				 break;
+				//AddFriend
+				cout<<"ask to be friend!\n";
+				job.Get("UserID", s1);
+				job.Get("FriendID", s2);
+				int result=DB.AskFriend(s1.c_str(),s2.c_str());
+				ret.Add("type", type);
+				if(result==100){
+					ret.Add("status", 100);
+					ret.Add("FriendID", s1);
+				}
+				else if(result==-1){
+					ret.Add("status", 0);
+				}
+				else{
+					ret.Add("status", 1);
+					ret.Add("FriendID", s1);
+				}
+				break;
 			}		   
 			case 5:{
-				//Invite
-				 break;
+				//join
+				cout<<"join\n";
+				job.Get("UserID", s1);
+				job.Get("GroupID", s2);
+				int result=DB.Join(s1.c_str(),s2.c_str());
+				ret.Add("type", type);
+				if(result==100){
+					ret.Add("status",100);
+					ret.Add("FriendID", s1);
+				}
+				else if(result==101){
+					ret.Add("status", 101);
+					ret.Add("GroupID", s2);
+				}
+				else if(result==-1){
+					ret.Add("status", 0);
+				}
+				else{
+					ret.Add("status", 1);
+					ret.Add("FriendID", s1);
+					ret.Add("GroupID", s2);
+				}
+				break;
 			}		   
 			case 6:{
-				//send message
-				 break;
+				break;
 			}
 			case 7:{
-				//be friend / be 
 				break;
-			}	   
+			}
+			default:{
+				cout<<"unkwown"<<hvie;
+				
+			}		
 		}
+		sprintf(buf, ret.ToString().c_str());
+		write(cid, buf, BUFFER_SIZE);
 	}
 }
 int main(){
 	code["register"]=1;
 	code["creategroup"]=3;
+	code["befriend"]=4;
+	code["join"]=5;
+	code["invite"]=6;
+	
 	pthread_t tid;
 	memset(&myAddr,0,sizeof(myAddr));
 	Len=sizeof(myAddr);
